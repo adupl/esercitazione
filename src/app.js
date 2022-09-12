b48ad3efc669238961f0177b7c05ccbe54161dc8
Perrino <simone.perrino@accenture.com>
2022-09-12 16:25:22 +0200

    servizi

--- src/app.js
+++ src/app.js
@@ -5,8 +5,11 @@ import './assets/styles/main.scss';
 import '../src/components/accordion/accordion';
 import '../src/components/dropdown/dropdown';
 import '../src/components/carousel/carousel';
+import '../src/components/servizi/servizi';
 
 
 $(document).ready(() => {
 	console.log('Start GLOBAL JS');	
 });
+
+
