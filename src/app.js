4b9de99d0f18bfc2fa5b2ca528cef7bbd95bbcbb
DIR\antonio.calise <antonio.calise@accenture.com>
2022-09-12 12:17:54 +0200

    navbar

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ import './assets/styles/main.scss';
 import '../src/components/accordion/accordion';
 import '../src/components/dropdown/dropdown';
 import '../src/components/carousel/carousel';
-
+import '../src/components/navbarTim/navbarTim';
 
 $(document).ready(() => {
 	console.log('Start GLOBAL JS');	
