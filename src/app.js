c6b29a1217cab7cce78e995c5cdc438d5a794dcf
Oppo <armando.maria.oppo@accenture.com>
2022-09-12 16:40:46 +0200

    banner-promozioni

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ import './assets/styles/main.scss';
 import '../src/components/accordion/accordion';
 import '../src/components/dropdown/dropdown';
 import '../src/components/carousel/carousel';
+import '../src/components/banner-promozioni/banner-promozioni';
 
 
 $(document).ready(() => {
