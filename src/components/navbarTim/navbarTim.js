3ee2671903833c97dc3fa0e1e5aeab832abe66d0
DIR\antonio.calise <antonio.calise@accenture.com>
2022-09-22 15:02:15 +0200

    fix css navbar incomplete

--- src/components/navbarTim/navbarTim.js
+++ src/components/navbarTim/navbarTim.js
@@ -8,14 +8,21 @@ $(document).ready(() => {
         console.log('Start JS navbar example');
        
         $('.under-navbar-item ').on('show.bs.dropdown', (e)=> {
-            console.log('puntatore dropdown:',  $(e.currentTarget).find('.under-nav-togg'));
-       
             $(e.currentTarget).find('.under-nav-togg').addClass('active');
         });
+
         $('.under-navbar-item ').on('hide.bs.dropdown', (e)=> {
-            console.log('puntatore dropdown chiuso:',  $(e));
-       
             $(e.currentTarget).find('.under-nav-togg').removeClass('active');
         });
+
+        $('.upper-option').on('click', (e)=> {
+            $('.upper-option').removeClass('option-active');
+            $(e.currentTarget).addClass('option-active');
+        });
+
+
+
+
+
     }
 });
