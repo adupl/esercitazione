f9f39706aa597d11627796a914d4af784daf522d
DIR\antonio.calise <antonio.calise@accenture.com>
2022-09-27 17:04:07 +0200

    undernav

--- src/components/navbarTim/navbarTim.js
+++ src/components/navbarTim/navbarTim.js
@@ -7,14 +7,13 @@ $(document).ready(() => {
         
         console.log('Start JS navbar example', $('.under-navbar-items.mobile .under-navbar-item'));
        
-        $('.under-navbar-item ').on('show.bs.dropdown', (e)=> {
+        /* $('.under-navbar-item ').on('show.bs.dropdown', (e)=> {
             $(e.currentTarget).find('.under-nav-togg').addClass('active');
         });
 
         $('.under-navbar-item ').on('hide.bs.dropdown', (e)=> {
             $(e.currentTarget).find('.under-nav-togg').removeClass('active');
-        });
-
+        });*/
         $('.upper-option').on('click', (e)=> {
             $('.upper-option').removeClass('option-active');
             $(e.currentTarget).addClass('option-active');
