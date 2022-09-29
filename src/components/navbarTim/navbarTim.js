6637d0bbb0b527bef2766ed501007d770bda5677
Alfonso, Duplice <alfonso.duplice@accenture.com>
2022-09-29 12:20:20 +0200

    last fix

--- src/components/navbarTim/navbarTim.js
+++ src/components/navbarTim/navbarTim.js
@@ -7,21 +7,14 @@ $(document).ready(() => {
         
         console.log('Start JS navbar example', $('.under-navbar-items.mobile .under-navbar-item'));
        
-        /* $('.under-navbar-item ').on('show.bs.dropdown', (e)=> {
-            $(e.currentTarget).find('.under-nav-togg').addClass('active');
-        });
-
-        $('.under-navbar-item ').on('hide.bs.dropdown', (e)=> {
-            $(e.currentTarget).find('.under-nav-togg').removeClass('active');
-        });*/
         $('.upper-option').on('click', (e)=> {
             $('.upper-option').removeClass('option-active');
             $(e.currentTarget).addClass('option-active');
         });
 
         $('#mobile-drop-wrapper').on('show.bs.collapse','.dropTim-mobile', function(e) {
-            if(e.currentTarget.id === 'mobile-drop-wrapper'){
-            $('#mobile-drop-wrapper').find('.dropTim-mobile.collapse.show').collapse('hide');
+            if($(e.currentTarget).hasClass('dropTim-mobile')){
+                $('#mobile-drop-wrapper').find('.dropTim-mobile.collapse.show').collapse('hide');
             }
         });
 
